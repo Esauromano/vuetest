327f5ce2440ebc02dda217b68951633d066338d0
jay <esauromano@gmail.com>
2020-10-22 18:45:36 -0500

    Removing lazy loading

--- vanglar/src/router/index.js
+++ vanglar/src/router/index.js
@@ -21,18 +21,12 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: Login
   },
   {
     path: '/signup',
     name: 'SignUp',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/SignUp.vue')
+    component: SignUp
   }
 ]
 
