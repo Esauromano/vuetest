8d083c2dbffb9dd99e22814eeb5f4bc41d26c1fd
jay <esauromano@gmail.com>
2020-10-22 23:55:20 -0500

    Adding a page for the Graph

---
+++ vanglar/src/views/Graph.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="graph">
+    <h1>This is an about page</h1>
+    <button @click="load">Login</button>
+  </div>
+</template>
+<script>
+import firebase from 'firebase'
+const db = firebase
+  .initializeApp({ databaseURL: 'https://vanglar-fa637.firebaseio.com/' })
+  .database()
+  export default {
+    name: 'graph',
+    methods: {
+      load: function() {
+        // retrieve a collection
+        db.ref('anglar-fa637').once('value', snapshot => {
+          const docs = snapshot.val()
+          // do something with documents
+          console.log(docs)
+        })
+      }
+    }
+  }
+</script>
--- vanglar/src/views/Home.vue
+++ vanglar/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vanglar App"/>
+    
     <button @click="logout">Logout</button>
   </div>
 </template>
