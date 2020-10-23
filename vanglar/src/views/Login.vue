8c40b43da8f3d022cb615618a7e71ca6ccda1611
jay <esauromano@gmail.com>
2020-10-22 19:20:34 -0500

    Connecting Login to Firebase

--- vanglar/src/views/Login.vue
+++ vanglar/src/views/Login.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an Login page</h1>
+  <div class="Login">
+    <h3>Sign In</h3>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button @click="login">Login</button>
+    <p>You don't have an account? You can <router-link to="/signup">create one</router-link></p>
   </div>
 </template>
+<style lang="css">
+.Login{
+  display: flex;
+  flex-direction: column;
+  max-width: 30vw;
+  margin: auto;
+}
+</style>
+<script>
+  import firebase from 'firebase';
+  export default {
+    name: 'login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      login: function() {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          () => {
+            this.$router.replace('home')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        );
+      }
+    }
+  }
+</script>
